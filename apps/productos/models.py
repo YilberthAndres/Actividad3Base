73330fbca39d84e8e78eae090fe200967fe417c8
israel25D <ijesusdiaz@uniguajira.edu.co>
2021-09-22 23:44:07 -0500

    israel

--- apps/productos/models.py
+++ apps/productos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class products(models.Model):
+class Products(models.Model):
     codigo = models.CharField(max_length=100)
     desccion1 = models.CharField(max_length=100)
     precio = models.CharField(max_length=100)
