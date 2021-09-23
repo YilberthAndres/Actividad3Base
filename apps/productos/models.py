5b2c15695b4052d5f28a6d0b21cc2a74b6c179ed
Yilberth <yilberthgalarza444@gmail.com>
2021-09-22 21:57:57 -0500

    yilberth

--- apps/productos/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Productos(models.model):
-    descripcion  = models.CharField(max_length=100)
-    precio       = models.BigIntegerField()
-    Nexistencias = models.IntegerField()
-    
