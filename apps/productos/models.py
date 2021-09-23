97333c48c84bb8e657ade8d2730d801c5491bda0
israel25D <ijesusdiaz@uniguajira.edu.co>
2021-09-22 22:20:06 -0500

    israel

--- apps/productos/models.py
+++ apps/productos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.compras.models import Compras
+
 
 # Create your models here.
+class products(models.Model):
+    codigo = models.CharField(max_length=100)
+    desccion1 = models.CharField(max_length=100)
+    precio = models.CharField(max_length=100)
+    numero_existencia = models.CharField(max_length=100)
+    
+    compras = models.ManyToManyField(Compras)
