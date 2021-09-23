0425476d2a1ec1b1d39d34915f57dd5c2b42b650
LeinysPaola <lepagifo@hotmail.com>
2021-09-22 22:57:58 -0500

    leinys

--- apps/compras/models.py
+++ apps/compras/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from app.clients.models import Clients
+from app.productos.models import Productos
+
 
 # Create your models here.
+class Compras(models.Mmodel):
+    fecha = models.DateField()
+    registro= models.CharField(max_length=100)
+    clients = models.ForeingKey(Clients)
+    productos = models.Foreingkey(Productos)
--- apps/proveedores/models.py
+++ apps/proveedores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from apps.productos.models import Productos
 
 # Create your models here.
+class Proveedores(models.Mmodel):
+    nombre = models.CharField(max_length=100)
+    apellido= models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    provincia = models.CharField(max_length=100)
+    telefono = models.IntegerField(max_length=20)
+    productos = models.ForeingKey(Productos)
